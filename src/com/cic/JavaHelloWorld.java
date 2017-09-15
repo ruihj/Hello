7ba157db80129632e0514822fb8fe0c4231d4b9c
Hong jian Rui <hjrui@cn.ibm.com>
2017-09-15 17:02:50 +0800

    test by rui

---
+++ src/com/cic/JavaHelloWorld.java
@@ -0,0 +1,10 @@
+package com.cic;
+
+public class JavaHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Jekins&HelloWorld");
+	}
+
+}
